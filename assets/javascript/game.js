bb3a2d498fc3c3a64aece16740cbaefabd0c1454
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-20 20:23:27 -0400

    Made all of the click events one function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,6 @@ var computerNumb = getRndInteger(19, 120);
 var wins = 0;
 var losses = 0;
 var score = 0;
-var buttonClicked = true;
-
 
 function startGame() {
     var wins = 0;
@@ -19,31 +17,9 @@ function startGame() {
     var button3 = getRndInteger(1, 12);
     var button4 = getRndInteger(1, 12);
     var computerNumb = getRndInteger(19, 120);
-    var buttonClicked = true;
 }
 
-
-function resetGame() {
-    var button1 = getRndInteger(1, 12);
-    var button2 = getRndInteger(1, 12);
-    var button3 = getRndInteger(1, 12);
-    var button4 = getRndInteger(1, 12);
-    var computerNumb = getRndInteger(19, 120);
-    var wins = 0;
-    var losses = 0;
-    var score = 0;
-    var buttonClicked = true;
-}
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-// console.log(getRndInteger(19,120));
-
-$(document).ready(function () {
-
-    $("#randomNumb").text(computerNumb);
-
+function clickButton () {
     $("#box1").on("click", function () {
         score = score + button1;
         $("#yourScore").text(score);
@@ -52,6 +28,7 @@ $(document).ready(function () {
     $("#box2").on("click", function () {
         score = score + button2;
         $("#yourScore").text(score);
+        
     });
 
     $("#box3").on("click", function () {
@@ -64,14 +41,42 @@ $(document).ready(function () {
         $("#yourScore").text(score);
     });
 
-
     if (score === computerNumb) {
-        $("#youWin").text()
+        wins++;
+        resetGame();
     }
 
-    else if (score >= computerNumb) {
-
+    else {
+        losses++;
+        resetGame();
     }
+}
+
+
+function resetGame() {
+    var button1 = getRndInteger(1, 12);
+    var button2 = getRndInteger(1, 12);
+    var button3 = getRndInteger(1, 12);
+    var button4 = getRndInteger(1, 12);
+    var computerNumb = getRndInteger(19, 120);
+    var wins = 0;
+    var losses = 0;
+    var score = 0;
+    $("#yourScore").text(score);
+    $("#randomNumb").text(computerNumb);
+
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+// console.log(getRndInteger(19,120));
+
+$(document).ready(function () {
+
+    $("#randomNumb").text(computerNumb);
+
+    clickButton();
 
 
 });
