d40b1c9bcbd78e37a9493d592e0a87ab9bfdfc40
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-21 09:52:46 -0400

    Added function to make the game reset when you win or lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,64 +8,114 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
-function startGame() {
-    var wins = 0;
-    var losses = 0;
-    var score = 0;
-    var button1 = getRndInteger(1, 12);
-    var button2 = getRndInteger(1, 12);
-    var button3 = getRndInteger(1, 12);
-    var button4 = getRndInteger(1, 12);
-    var computerNumb = getRndInteger(19, 120);
-}
+// function startGame() {
+//     var wins = 0;
+//     var losses = 0;
+//     var score = 0;
+//     var button1 = getRndInteger(1, 12);
+//     var button2 = getRndInteger(1, 12);
+//     var button3 = getRndInteger(1, 12);
+//     var button4 = getRndInteger(1, 12);
+//     var computerNumb = getRndInteger(19, 120);
+// }
 
-function clickButton () {
+function clickButton() {
     $("#box1").on("click", function () {
         score = score + button1;
         $("#yourScore").text(score);
+
+        if (score == computerNumb) {
+            wins++;
+            $("#wins").text(wins);
+            resetGame();
+        }
+    
+        else if (score > computerNumb) {
+            losses++;
+            $("#losses").text(losses);
+            resetGame();
+        }
+    
+
+
     });
 
     $("#box2").on("click", function () {
         score = score + button2;
         $("#yourScore").text(score);
-        
+
+        if (score == computerNumb) {
+            wins++;
+            $("#wins").text(wins);
+            resetGame();
+        }
+    
+        else if (score > computerNumb) {
+            losses++;
+            $("#losses").text(losses);
+            resetGame();
+        }
     });
 
     $("#box3").on("click", function () {
         score = score + button3;
         $("#yourScore").text(score);
+        if (score === computerNumb) {
+            wins++;
+            $("#wins").text(wins);
+            resetGame();
+        }
+    
+        else if (score > computerNumb) {
+            losses++;
+            $("#losses").text(losses);
+            resetGame();
+        }
     });
 
     $("#box4").on("click", function () {
         score = score + button4;
         $("#yourScore").text(score);
+        if (score == computerNumb) {
+            wins++;
+            $("#wins").text(wins);
+            resetGame();
+        }
+    
+        else if (score > computerNumb) {
+            losses++;
+            $("#losses").text(losses);
+            resetGame();
+        }
     });
-
-    if (score === computerNumb) {
-        wins++;
-        resetGame();
-    }
-
-    else {
-        losses++;
-        resetGame();
-    }
 }
 
 
 function resetGame() {
-    var button1 = getRndInteger(1, 12);
-    var button2 = getRndInteger(1, 12);
-    var button3 = getRndInteger(1, 12);
-    var button4 = getRndInteger(1, 12);
-    var computerNumb = getRndInteger(19, 120);
-    var wins = 0;
-    var losses = 0;
-    var score = 0;
+     button1 = getRndInteger(1, 12);
+     button2 = getRndInteger(1, 12);
+     button3 = getRndInteger(1, 12);
+     button4 = getRndInteger(1, 12);
+     computerNumb = getRndInteger(19, 120);
+     wins = 0;
+     losses = 0;
+     score = 0;
     $("#yourScore").text(score);
     $("#randomNumb").text(computerNumb);
-
 }
+
+function youWon() {
+    $("#youWin").text("GOOD JOB!");
+    wins++;
+    $("#youWin").text(wins);
+}
+
+function youLost() {
+    $("#youLose").text("NOT A VERY GOOD JOB!");
+    losses++;
+    $("#youLose").text(losses);
+}
+
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -77,6 +127,7 @@ $(document).ready(function () {
     $("#randomNumb").text(computerNumb);
 
     clickButton();
+
 
 
 });
