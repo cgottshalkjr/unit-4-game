cba70bd750859171ff3c26eb9f34e945a0a538dd
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-21 18:51:54 -0400

    CSS styling added and got the wins and losses tallying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,18 +8,9 @@ var wins = 0;
 var losses = 0;
 var score = 0;
 
-// function startGame() {
-//     var wins = 0;
-//     var losses = 0;
-//     var score = 0;
-//     var button1 = getRndInteger(1, 12);
-//     var button2 = getRndInteger(1, 12);
-//     var button3 = getRndInteger(1, 12);
-//     var button4 = getRndInteger(1, 12);
-//     var computerNumb = getRndInteger(19, 120);
-// }
-
+//This function contains all four button clicks.
 function clickButton() {
+
     $("#box1").on("click", function () {
         score = score + button1;
         $("#yourScore").text(score);
@@ -29,31 +20,30 @@ function clickButton() {
             $("#wins").text(wins);
             resetGame();
         }
-    
+
         else if (score > computerNumb) {
             losses++;
             $("#losses").text(losses);
             resetGame();
+
         }
-    
-
-
     });
 
     $("#box2").on("click", function () {
         score = score + button2;
         $("#yourScore").text(score);
-
         if (score == computerNumb) {
             wins++;
             $("#wins").text(wins);
+
             resetGame();
         }
-    
+
         else if (score > computerNumb) {
             losses++;
             $("#losses").text(losses);
             resetGame();
+
         }
     });
 
@@ -65,11 +55,12 @@ function clickButton() {
             $("#wins").text(wins);
             resetGame();
         }
-    
+
         else if (score > computerNumb) {
             losses++;
             $("#losses").text(losses);
             resetGame();
+
         }
     });
 
@@ -81,41 +72,26 @@ function clickButton() {
             $("#wins").text(wins);
             resetGame();
         }
-    
+
         else if (score > computerNumb) {
             losses++;
             $("#losses").text(losses);
             resetGame();
+
         }
     });
 }
 
-
 function resetGame() {
-     button1 = getRndInteger(1, 12);
-     button2 = getRndInteger(1, 12);
-     button3 = getRndInteger(1, 12);
-     button4 = getRndInteger(1, 12);
-     computerNumb = getRndInteger(19, 120);
-     wins = 0;
-     losses = 0;
-     score = 0;
+    button1 = getRndInteger(1, 12);
+    button2 = getRndInteger(1, 12);
+    button3 = getRndInteger(1, 12);
+    button4 = getRndInteger(1, 12);
+    computerNumb = getRndInteger(19, 120);
+    score = 0;
     $("#yourScore").text(score);
     $("#randomNumb").text(computerNumb);
 }
-
-function youWon() {
-    $("#youWin").text("GOOD JOB!");
-    wins++;
-    $("#youWin").text(wins);
-}
-
-function youLost() {
-    $("#youLose").text("NOT A VERY GOOD JOB!");
-    losses++;
-    $("#youLose").text(losses);
-}
-
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -126,9 +102,10 @@ $(document).ready(function () {
 
     $("#randomNumb").text(computerNumb);
 
+    $("#losses").text(losses);
+    $("#wins").text(wins);
+
     clickButton();
-
-
 
 });
 
@@ -137,15 +114,3 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
-
-
-//Pseudo Code 
-// When the game is loaded there should be a random computer score
-//At the same time all four buttons should generate a random hidden score
-//When user clicks a button the random scoreshould apply to yourScore
-//User continues to click buttons to try and match the score 
